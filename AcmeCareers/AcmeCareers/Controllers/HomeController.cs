ca94f45103fedae9870fd14f0760c4cfe55a9b43
Dhaval Tejlavwala <dhavaltejlavwala@hotmail.com>
2018-05-13 23:56:06 +1000

    Part 1 Done

--- AcmeCareers/AcmeCareers/Controllers/HomeController.cs
+++ AcmeCareers/AcmeCareers/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AcmeCareers.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,59 @@ namespace AcmeCareers.Controllers
     {
         public ActionResult Index()
         {
+            var db = new ApplicationDbContext();
+            IEnumerable<SelectListItem> items = db.JobInfo.Select(c => new SelectListItem
+            {
+                Value = c.Title + "|" + c.JobId,
+                Text = c.Title
+
+            });
+            ViewBag.JobTitleList = items;
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(JobApplication Model)
+        {
+            try
+            {
+                if (ModelState.IsValid && Model != null && !String.IsNullOrEmpty(Model.Name) && !String.IsNullOrEmpty(Model.Email) && !String.IsNullOrEmpty(Model.JobTitle) && !String.IsNullOrEmpty(Model.Comments)) //&& ((Model.JobId.JobId.GetType() == typeof(int)))
+                {
+                    char[] splitchar = { '|' };
+                    var splitJobTitle = Model.JobTitle.Split(splitchar);
+
+                    var Name = Model.Name;
+                    var Email = Model.Email;
+                    var JobTitle = splitJobTitle[0];
+                    var Comments = Model.Comments;
+                    var JobId = Convert.ToInt16(splitJobTitle[1]);
+                    var CurrentTime = DateTime.Now;
+
+                    var db = new ApplicationDbContext();
+                    var InsertApplicationForm = new JobApplication { Name = Name, Email = Email, JobTitle = JobTitle, Comments = Comments, AppDateTime = CurrentTime, JobId = JobId };
+                    db.JobApplication.Add(InsertApplicationForm);
+                    db.SaveChanges();
+
+                    ViewBag.Class = "success";
+                    ViewBag.Strong = "Thank you";
+                    ViewBag.Message = " for your application";
+                    return PartialView("_AppAck");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Class = "danger";
+                ViewBag.Strong = "Error";
+                ViewBag.Message = "Something went wrong! <br /> Please Try Again <br /> <br />" + ex.ToString();
+                return PartialView("_AppAck");
+            }
+
+            ViewBag.Class = "danger";
+            ViewBag.Strong = "Error";
+            ViewBag.Message = "Invalid data! <br /> Please Try Again <br /> <br />";
+            return PartialView("_AppAck");
         }
 
         public ActionResult About()
---
+++ AcmeCareers/AcmeCareers/Controllers/JobInfoController.cs
@@ -0,0 +1,106 @@
+﻿using AcmeCareers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcmeCareers.Controllers
+{
+    public class JobInfoController : Controller
+    {
+        // GET: JobInfo
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var viewModel = new JobInfo();
+            var query = from job in db.JobInfo
+                        select job;
+            return View(query);
+        }
+
+        // GET: JobInfo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: JobInfo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: JobInfo/Create
+        [HttpPost]
+        public ActionResult Create(JobInfo Model)
+        {
+            try
+            {
+                if (Model != null && !String.IsNullOrEmpty(Model.Title) && !String.IsNullOrEmpty(Model.Location) && !String.IsNullOrEmpty(Model.ContactPerson))
+                {
+
+                    var Title = Model.Title;
+                    var Location = Model.Location;
+                    var ContactPerson = Model.ContactPerson;
+                    var db = new ApplicationDbContext();
+                    var InsertJobInfo = new JobInfo { Title = Title, Location = Location, ContactPerson = ContactPerson };
+                    db.JobInfo.Add(InsertJobInfo);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return RedirectToAction("Create");
+            }
+
+            return RedirectToAction("Create");
+        }
+
+        // GET: JobInfo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: JobInfo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: JobInfo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: JobInfo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
